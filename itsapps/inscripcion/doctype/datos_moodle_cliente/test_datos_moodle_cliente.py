cce0014dcfeb97839eda9bc5d3fc3c940cee4f77
QPRodrigo <rodrigoqp0722@gmail.com>
2022-10-14 15:58:09 +0000

    Datos Moodle Cliente Creado

---
+++ itsapps/inscripcion/doctype/datos_moodle_cliente/test_datos_moodle_cliente.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2022, QPRodrigo and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestDatosMoodleCliente(FrappeTestCase):
+	pass
